a3979737eff6efc31814ca7602a35dfdbba99446
Marzena <marzena.szopinska090@gmail.com>
2019-04-08 08:23:50 +0200

    added external file that contains data for the to do list

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ToDoItem() {
   return (
     <div className='item'>
-      <input type='checkbox' id='shopping' name='to-do-user' value='shopping' />
+      <input type='checkbox' id='shopping' />
       <label for='shopping'>Do Shopping</label>
       <br />
     </div>
---
+++ src/todosData.js
@@ -0,0 +1,20 @@
+
+const todosData = [
+  {
+    id: 1,
+    text: 'Take out the trash',
+    completed: true;
+  },
+  {
+    id: 2,
+    text: 'Do shopping',
+    completed: false;
+  },
+  {
+    id: 3,
+    text: 'Mow lawn',
+    completed: false;
+  }
+];
+
+export default todosData;
